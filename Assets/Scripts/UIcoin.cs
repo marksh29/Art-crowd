7dd5660f6acdc4290ce2e36b7446ab3eb048c68b
marksh29 <31987208+marksh29@users.noreply.github.com>
2022-04-21 18:47:06 +0800

    21.04.22 #2
    Конфиг монеток

--- Assets/Scripts/UIcoin.cs
+++ Assets/Scripts/UIcoin.cs
@@ -8,6 +8,11 @@ public class UIcoin : MonoBehaviour
     [SerializeField] Transform[] moveCoins;
     [SerializeField] Transform targetPos;
     [SerializeField] float moveTime;
+
+    //float x;
+    //float y;
+    //float z;
+
     void Start()
     {
         if (Instance == null)
@@ -26,6 +31,12 @@ public class UIcoin : MonoBehaviour
         Vector3 stPos = Camera.main.WorldToScreenPoint(pos.position);
         for (int i = 0; i < count; i++)
         {
+
+            //x = Random.Range(-100, 100); Для разброса при появлении
+            //y = Random.Range(-100, 100);
+            //z = 0;
+            //stPos = stPos + new Vector3(x, y, z);
+
             moveCoins[i].position = stPos;
             moveCoins[i].gameObject.SetActive(true);
             StartCoroutine(DoMove(moveCoins[i]));
@@ -34,14 +45,14 @@ public class UIcoin : MonoBehaviour
     }
     IEnumerator Scale()
     {
-        targetPos.localScale = new Vector3(1f, 1f, 1f);
-        targetPos.localScale = new Vector3(1.3f, 1.3f, 1.3f);
-        yield return new WaitForSeconds(0.1f);
+        targetPos.localScale = new Vector3(1.4f, 1.4f, 1.4f); //Перед этим скейл до 1f,1f,1f убрал, чтоб чище анимация пульсации была
+        yield return new WaitForSeconds(0.05f);
         targetPos.localScale = new Vector3(1f, 1f, 1f);
     }
     private IEnumerator DoMove(Transform obj)
     {
         Vector2 startPosition = obj.localPosition;
+        //yield return new WaitForSeconds(0.5f); Чтоб повисели, потом отлетели
         float startTime = Time.realtimeSinceStartup;
         float fraction = 0f;
         while (fraction < 1f)
